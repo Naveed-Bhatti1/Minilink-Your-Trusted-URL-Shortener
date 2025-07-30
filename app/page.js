b847e399a06c87b075e40f94a2c8898f360d90b8
Naveed-Bhatti1 <codeholder1@gmail.com>
2025-07-30 15:35:40 +0500

    Readme added

--- app/page.js
+++ app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
             easy to share and manage.
           </p>
           <div>
-            <Link href={"/github"}>
+            <Link href={"https://github.com/Naveed-Bhatti1/Minilink-Your-Trusted-URL-Shortener"}>
             <button
               type="button"
               className="text-white mt-6 cursor-pointer bg-gradient-to-r from-blue-400  to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
--- components/Navbar.js
+++ components/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
             </span>
           </Link>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <Link href={"/github"}>
+            <Link href={"https://github.com/Naveed-Bhatti1/Minilink-Your-Trusted-URL-Shortener"}>
               <button
                 type="button"
                 className="text-white  cursor-pointer bg-gradient-to-r from-blue-500  to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
